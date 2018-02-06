9e6c0bec7d5c3749e5a234c10e0e0fbc0f5e2df9
Berry  Semexan <bsemexan@gmail.com>
2018-02-06 20:19:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,5 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
-  end
+  return "Fizz" if int % 3 == 0
   return "Buzz" if int % 5 == 0
   return "FizzBuzz" if int % 3 == 0 && int % 5 == 0
   return nil if int % 3 != 0 || int % 5 != 0
